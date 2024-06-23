156fd99b2622c389c0e11d1f4a1103e6d180ea81
Gabrieli Alves <117552601+castroalves-gabi@users.noreply.github.com>
2024-06-22 23:33:01 -0300

    Create 13-resolucao.py

---
+++ 13-resolucao.py
@@ -0,0 +1,41 @@
+class PlanoTelefone:
+    def __init__(self, nome, saldo):
+        self.__nome = nome
+        self.__saldo = saldo
+
+    def verificar_saldo(self):
+        return self.__saldo
+
+    def mensagem_personalizada(self):
+        saldo = self.verificar_saldo()
+        if saldo < 10:
+            return "Seu saldo está baixo. Recarregue e use os serviços do seu plano."
+        elif saldo >= 50:
+            return "Parabéns! Continue aproveitando seu plano sem preocupações."
+        else:
+            return "Seu saldo está razoável. Aproveite o uso moderado do seu plano."
+
+
+class UsuarioTelefone:
+    def __init__(self, nome, plano):
+        self.nome = nome
+        self.plano = plano
+
+    def verificar_saldo(self):
+        saldo = self.plano.verificar_saldo()
+        mensagem = self.plano.mensagem_personalizada()
+        return saldo, mensagem
+
+
+# Recebendo as entradas do usuário (nome, plano, saldo):
+nome_usuario = input().strip()
+nome_plano = input().strip()
+saldo_inicial = float(input().strip())
+
+# Criação de objetos do plano de telefone e usuário de telefone com dados fornecidos:
+plano_usuario = PlanoTelefone(nome_plano, saldo_inicial)
+usuario = UsuarioTelefone(nome_usuario, plano_usuario)
+
+# Chamada do método para verificar_saldo sem acessar diretamente os atributos do plano:
+saldo_usuario, mensagem_usuario = usuario.verificar_saldo()
+print(mensagem_usuario)
